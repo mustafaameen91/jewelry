4b6f213f8262ae09ee8caea29af1ac24ced7fca4
mustafaameen91 <mustafa.mohammed.ameen91@gmail.com>
2021-08-19 23:34:35 +0300

    add new routes

--- app/models/item.models.js
+++ app/models/item.models.js
@@ -1,152 +1,158 @@
 const sql = require("./db.js");
 
 function arrangeData(item, images, found) {
-  let itemImages = images.filter((image) => {
-    return image.itemId == item.idItem;
-  });
+   let itemImages = images.filter((image) => {
+      return image.itemId == item.idItem;
+   });
 
-  if (itemImages.length > 0) {
-    return {
-      idItem: item.idItem,
-      itemName: item.itemName,
-      itemDescription: item.itemDescription,
-      itemDate: item.itemDate,
-      itemQuality: item.itemQuality,
-      itemQuantity: item.itemQuantity,
-      itemLike: item.itemLike,
-      itemNameEn: item.itemNameEn,
-      itemDescriptionEn: item.itemDescriptionEn,
-      subName: item.subName,
-      categoryName: item.categoryName,
-      subNameEn: item.subNameEn,
-      categoryNameEn: item.categoryNameEn,
-      idSub: item.idSub,
-      foundFavorite: found,
-      images: itemImages,
-      isFavorite: !!item.isFavorite,
-      likes: item.favoritesCount,
-    };
-  } else {
-    return {
-      idItem: item.idItem,
-      itemName: item.itemName,
-      itemDescription: item.itemDescription,
-      itemDate: item.itemDate,
-      itemQuality: item.itemQuality,
-      itemQuantity: item.itemQuantity,
-      itemLike: item.itemLike,
-      itemNameEn: item.itemNameEn,
-      itemDescriptionEn: item.itemDescriptionEn,
-      subName: item.subName,
-      categoryName: item.categoryName,
-      subNameEn: item.subNameEn,
-      categoryNameEn: item.categoryNameEn,
-      idSub: item.idSub,
-      foundFavorite: found,
-      images: [],
-      isFavorite: !!item.isFavorite,
-      likes: item.favoritesCount,
-    };
-  }
+   if (itemImages.length > 0) {
+      return {
+         idItem: item.idItem,
+         itemName: item.itemName,
+         itemDescription: item.itemDescription,
+         itemDate: item.itemDate,
+         itemQuality: item.itemQuality,
+         itemQuantity: item.itemQuantity,
+         itemLike: item.itemLike,
+         itemNameEn: item.itemNameEn,
+         itemDescriptionEn: item.itemDescriptionEn,
+         subName: item.subName,
+         categoryName: item.categoryName,
+         subNameEn: item.subNameEn,
+         categoryNameEn: item.categoryNameEn,
+         idSub: item.idSub,
+         foundFavorite: found,
+         images: itemImages,
+         isFavorite: !!item.isFavorite,
+         likes: item.favoritesCount,
+      };
+   } else {
+      return {
+         idItem: item.idItem,
+         itemName: item.itemName,
+         itemDescription: item.itemDescription,
+         itemDate: item.itemDate,
+         itemQuality: item.itemQuality,
+         itemQuantity: item.itemQuantity,
+         itemLike: item.itemLike,
+         itemNameEn: item.itemNameEn,
+         itemDescriptionEn: item.itemDescriptionEn,
+         subName: item.subName,
+         categoryName: item.categoryName,
+         subNameEn: item.subNameEn,
+         categoryNameEn: item.categoryNameEn,
+         idSub: item.idSub,
+         foundFavorite: found,
+         images: [],
+         isFavorite: !!item.isFavorite,
+         likes: item.favoritesCount,
+      };
+   }
 }
 
 function foundFavorite(itemId, macAddress, arrayOfFavorite) {
-  console.log(arrayOfFavorite);
-  let found = arrayOfFavorite.filter((fav) => {
-    return fav.itemId == itemId && fav.macAddress == macAddress;
-  });
+   console.log(arrayOfFavorite);
+   let found = arrayOfFavorite.filter((fav) => {
+      return fav.itemId == itemId && fav.macAddress == macAddress;
+   });
 
-  if (found.length > 0) {
-    return true;
-  } else {
-    return false;
-  }
+   if (found.length > 0) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 const Item = function (item) {
-  this.itemName = item.itemName;
-  this.itemDescription = item.itemDescription;
-  this.itemQuality = item.itemQuality;
-  this.itemQuantity = item.itemQuantity;
-  this.itemNameEn = item.itemNameEn;
-  this.itemDescriptionEn = item.itemDescriptionEn;
+   this.itemName = item.itemName;
+   this.itemDescription = item.itemDescription;
+   this.itemQuality = item.itemQuality;
+   this.itemQuantity = item.itemQuantity;
+   this.itemNameEn = item.itemNameEn;
+   this.itemDescriptionEn = item.itemDescriptionEn;
 };
 
 Item.create = (newItem, result) => {
-  sql.query("INSERT INTO item SET ?", newItem, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
+   sql.query("INSERT INTO item SET ?", newItem, (err, res) => {
+      if (err) {
+         console.log("error: ", err);
+         result(err, null);
+         return;
+      }
 
-    console.log("created item: ", {id: res.insertId, ...newItem});
-    result(null, {id: res.insertId, ...newItem});
-  });
+      console.log("created item: ", { id: res.insertId, ...newItem });
+      result(null, { id: res.insertId, ...newItem });
+   });
 };
 
 Item.findById = (itemId, result) => {
-  sql.query(`SELECT * FROM item WHERE id = ${itemId}`, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
+   sql.query(`SELECT * FROM item WHERE id = ${itemId}`, (err, res) => {
+      if (err) {
+         console.log("error: ", err);
+         result(err, null);
+         return;
+      }
 
-    if (res.length) {
-      console.log("found item: ", res[0]);
-      result(null, res[0]);
-      return;
-    }
+      if (res.length) {
+         console.log("found item: ", res[0]);
+         result(null, res[0]);
+         return;
+      }
 
-    result({kind: "not_found"}, null);
-  });
+      result({ kind: "not_found" }, null);
+   });
 };
 
 Item.findBySubId = (subId, mac, result) => {
-  sql.query(
-    `SELECT item.idItem , item.itemName , item.itemDescription ,item.itemNameEn ,
+   sql.query(
+      `SELECT item.idItem , item.itemName , item.itemDescription ,item.itemNameEn ,
     item.itemDescriptionEn , DATE_FORMAT(item.itemDate, '%d/%m/%Y')AS itemDate ,
     item.itemQuality , item.itemQuantity , item.itemLike , subCategory.subName , 
     category.categoryName , category.categoryNameEn ,subCategory.subNameEn,
       count(f.idFavorites) as favoritesCount
-      ${mac ? ', IFNULL(NOT NOT(fs.itemId), FALSE) AS isFavorite' : ''}
+      ${mac ? ", IFNULL(NOT NOT(fs.itemId), FALSE) AS isFavorite" : ""}
     FROM item JOIN itemCategory JOIN subCategory JOIN category 
-    ON item.idItem = itemCategory.itemId AND category.idCategory = subCategory.categoryId 
-      ${mac ? "left JOIN favorites as fs ON item.idItem = fs.itemId AND fs.macAddress = '" + mac + "'" : ''} 
+    ON item.idItem = itemCategory.itemId AND category.idCategory = subCategory.categoryId  AND itemCategory.subId = subCategory.idSub
+      ${
+         mac
+            ? "left JOIN favorites as fs ON item.idItem = fs.itemId AND fs.macAddress = '" +
+              mac +
+              "'"
+            : ""
+      } 
     left join favorites as f on item.idItem = f.itemId 
     WHERE itemCategory.subId = ${subId} group by item.idItem, subCategory.idSub
     `,
-    (err, res) => {
-      if (err) {
-        return result(null, err);
-      }
-      sql.query(`SELECT * FROM image `, (err, resOne) => {
-        sql.query(`SELECT * FROM favorites`, (err, resFav) => {
-          let imageItem = res.map((item) => {
-            return arrangeData(
-              item,
-              resOne,
-              foundFavorite(item.idItem, mac, resFav)
-            );
-          });
-          if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-          }
+      (err, res) => {
+         if (err) {
+            return result(null, err);
+         }
+         sql.query(`SELECT * FROM image `, (err, resOne) => {
+            sql.query(`SELECT * FROM favorites`, (err, resFav) => {
+               let imageItem = res.map((item) => {
+                  return arrangeData(
+                     item,
+                     resOne,
+                     foundFavorite(item.idItem, mac, resFav)
+                  );
+               });
+               if (err) {
+                  console.log("error: ", err);
+                  result(null, err);
+                  return;
+               }
 
-          // console.log("item: ", res);
-          result(null, imageItem);
-        });
-      });
-    }
-  );
+               // console.log("item: ", res);
+               result(null, imageItem);
+            });
+         });
+      }
+   );
 };
 
 Item.getAll = (show, mac, result) => {
-  sql.query(
-    `SELECT
+   sql.query(
+      `SELECT
       item.idItem,
       item.itemName,
       subCategory.idSub,
@@ -162,7 +168,7 @@ Item.getAll = (show, mac, result) => {
       category.categoryNameEn,
       subCategory.subNameEn,
       count(f.idFavorites) as favoritesCount
-      ${mac ? ', IFNULL(NOT NOT(fs.itemId), FALSE) AS isFavorite' : ''}
+      ${mac ? ", IFNULL(NOT NOT(fs.itemId), FALSE) AS isFavorite" : ""}
    FROM
       item
       JOIN subCategory
@@ -170,101 +176,107 @@ Item.getAll = (show, mac, result) => {
       JOIN category ON itemCategory.itemId = item.idItem
          AND itemCategory.subId = subCategory.idSub
          AND subCategory.categoryId = category.idCategory
-      ${mac ? "left JOIN favorites as fs ON item.idItem = fs.itemId AND fs.macAddress = '" + mac + "'" : ''} 
+      ${
+         mac
+            ? "left JOIN favorites as fs ON item.idItem = fs.itemId AND fs.macAddress = '" +
+              mac +
+              "'"
+            : ""
+      } 
     left join favorites as f on item.idItem = f.itemId group by item.idItem, subCategory.idSub
     ${show}`,
-    (err, res) => {
-      if (err) {
-        result(null, err);
-      }
-      console.log('res:', res)
-      sql.query(`SELECT * FROM image `, (err, resOne) => {
-        let imageItem = res.map((item) => {
-          console.log('isFavorite:', item.isFavorite);
-          return arrangeData(item, resOne);
-        });
-        if (err) {
-          console.log("error: ", err);
-          result(null, err);
-          return;
-        }
+      (err, res) => {
+         if (err) {
+            result(null, err);
+         }
+         console.log("res:", res);
+         sql.query(`SELECT * FROM image `, (err, resOne) => {
+            let imageItem = res.map((item) => {
+               console.log("isFavorite:", item.isFavorite);
+               return arrangeData(item, resOne);
+            });
+            if (err) {
+               console.log("error: ", err);
+               result(null, err);
+               return;
+            }
 
-        // console.log("item: ", res);
-        result(null, imageItem);
-      });
-    }
-  );
+            // console.log("item: ", res);
+            result(null, imageItem);
+         });
+      }
+   );
 };
 
 Item.updateById = (id, item, result) => {
-  sql.query("UPDATE item SET ? WHERE id = ?", [item, id], (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(null, err);
-      return;
-    }
-
-    if (res.affectedRows == 0) {
-      result({kind: "not_found"}, null);
-      return;
-    }
-
-    console.log("updated item: ", {id: id, ...item});
-    result(null, {id: id, ...item});
-  });
-};
-
-Item.updateLikeById = (id, itemLike, result) => {
-  sql.query(
-    "UPDATE item SET itemLike  = ? WHERE idItem = ?",
-    [itemLike + 1, id],
-    (err, res) => {
+   sql.query("UPDATE item SET ? WHERE id = ?", [item, id], (err, res) => {
       if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
+         console.log("error: ", err);
+         result(null, err);
+         return;
       }
 
       if (res.affectedRows == 0) {
-        result({kind: "not_found"}, null);
-        return;
+         result({ kind: "not_found" }, null);
+         return;
       }
 
-      console.log("updated item: ", {id: id, ...itemLike});
-      result(null, {id: id, ...itemLike});
-    }
-  );
+      console.log("updated item: ", { id: id, ...item });
+      result(null, { id: id, ...item });
+   });
+};
+
+Item.updateLikeById = (id, itemLike, result) => {
+   sql.query(
+      "UPDATE item SET itemLike  = ? WHERE idItem = ?",
+      [itemLike + 1, id],
+      (err, res) => {
+         if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+         }
+
+         if (res.affectedRows == 0) {
+            result({ kind: "not_found" }, null);
+            return;
+         }
+
+         console.log("updated item: ", { id: id, ...itemLike });
+         result(null, { id: id, ...itemLike });
+      }
+   );
 };
 
 Item.remove = (id, result) => {
-  sql.query("DELETE FROM item WHERE idItem = ?", id, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(null, err);
-      return;
-    }
+   sql.query("DELETE FROM item WHERE idItem = ?", id, (err, res) => {
+      if (err) {
+         console.log("error: ", err);
+         result(null, err);
+         return;
+      }
 
-    if (res.affectedRows == 0) {
-      result({kind: "not_found"}, null);
-      return;
-    }
+      if (res.affectedRows == 0) {
+         result({ kind: "not_found" }, null);
+         return;
+      }
 
-    console.log("deleted item with id: ", id);
-    result(null, res);
-  });
+      console.log("deleted item with id: ", id);
+      result(null, res);
+   });
 };
 
 Item.removeAll = (result) => {
-  sql.query("DELETE FROM item", (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(null, err);
-      return;
-    }
+   sql.query("DELETE FROM item", (err, res) => {
+      if (err) {
+         console.log("error: ", err);
+         result(null, err);
+         return;
+      }
 
-    console.log(`deleted ${res.affectedRows} items`);
-    result(null, res);
-  });
+      console.log(`deleted ${res.affectedRows} items`);
+      result(null, res);
+   });
 };
 
 module.exports = Item;
