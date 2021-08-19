0db558f16b951d8200c6394580280536b023dc3c
mustafaameen91 <mustafa.mohammed.ameen91@gmail.com>
2021-08-19 21:18:02 +0300

    add new routes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
    HOST: "localhost",
    // USER: "root",
    // PASSWORD: ">7}a_TPjKzE}gsM}",
-   USER: "haider",
+   USER: "root",
    PASSWORD: "Hh123123!@",
    DB: "jewelry",
    PORT: 3306,
