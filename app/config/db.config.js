c4f35b5533e387512acdd2e01e2fbfac0abaafa1
mustafaameen91 <mustafa.mohammed.ameen91@gmail.com>
2021-11-01 12:07:42 +0300

    add resize image

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
    HOST: "localhost",
-   // USER: "root",
-   // PASSWORD: ">7}a_TPjKzE}gsM}",
    USER: "root",
-   PASSWORD: "root",
+   PASSWORD: ">7}a_TPjKzE}gsM}",
+   // USER: "root",
+   // PASSWORD: "root",
    DB: "Jewelry",
    PORT: 8889,
 };
