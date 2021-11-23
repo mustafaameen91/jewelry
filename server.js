3d58c1d63d73c936d8ac7a03e12b13b4bb58d4dd
mustafaameen91 <mustafa.mohammed.ameen91@gmail.com>
2021-11-23 21:07:17 +0300

    increase file size

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const notification = require("./app/notifications/notification");
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use(upload());
 
 require("./app/routes/category.routes.js")(app);
